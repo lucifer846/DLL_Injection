6cf90940db463f0609f4fc22fe25bd904a0c9404
Saurabh <151426154+lucifer846@users.noreply.github.com>
2025-02-27 07:45:55 +0530

    added a reference to dllpayload.cpp

--- dllpayload.cpp
+++ dllpayload.cpp
@@ -8,3 +8,27 @@ BOOL WINAPI DllMain(HINSTANCE hModule, DWORD Reason, LPVOID lpvReserved) {
 	}
 	return TRUE;
 }
+/*    // sample dll code for reference purposes
+BOOL APIENTRY DllMain(
+    HANDLE hModule,             // Handle to DLL module
+    DWORD ul_reason_for_call,   // Reason for calling function
+    LPVOID lpReserved           // Reserved
+) {
+    
+    switch (ul_reason_for_call) {
+        case DLL_PROCESS_ATTACHED: // A process is loading the DLL.
+        // Do something here
+        break;
+        case DLL_THREAD_ATTACHED: // A process is creating a new thread.
+        // Do something here
+        break;
+        case DLL_THREAD_DETACH: // A thread exits normally.
+        // Do something here
+        break;
+        case DLL_PROCESS_DETACH: // A process unloads the DLL.
+        // Do something here
+        break;
+    }
+    return TRUE;
+}
+*/
